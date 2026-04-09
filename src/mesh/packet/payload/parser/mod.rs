2d6a2ce52588d1bf44dcd29922df5cfc7d3308cc
Nareshkumar Rao <dev@nrx.sh>
2026-04-10 00:21:28 +0200

    wip

--- src/mesh/packet/payload/parser/mod.rs
+++ src/mesh/packet/payload/parser/mod.rs
@@ -1,14 +1,17 @@
 mod tests;
 
-use crate::mesh::{
-    contacts::Contacts,
-    identity::{LocalIdentity, RemoteIdentity},
-    packet::{
-        encryption::decrypt,
-        node::{NodeType, NodeTypeSet},
-        path::Path,
-        payload::{ControlData, Payload},
-        raw::PayloadType,
+use crate::{
+    error::{ParserError, ParserResult},
+    mesh::{
+        contacts::Contacts,
+        identity::{LocalIdentity, RemoteIdentity},
+        packet::{
+            encryption::decrypt,
+            node::{NodeType, NodeTypeSet},
+            path::Path,
+            payload::{ControlData, Payload},
+            raw::{MAX_PACKET_PAYLOAD, MAX_PATH_SIZE, PayloadType},
+        },
     },
 };
 use bilge::prelude::*;
@@ -19,7 +22,7 @@ pub struct PayloadParser {
 }
 
 impl PayloadParser {
-    pub fn parse(&self, data: &[u8], payload_type: PayloadType) -> Option<Payload> {
+    pub fn parse(&self, data: &[u8], payload_type: PayloadType) -> ParserResult<Payload> {
         let payload = match payload_type {
             PayloadType::Trace => Self::parse_trace(data)?,
             PayloadType::Control => Self::parse_control(data)?,
@@ -35,15 +38,21 @@ impl PayloadParser {
             PayloadType::AnonymousRequest => todo!(),
             PayloadType::RawCustom => todo!(),
         };
-        Some(payload)
+        Ok(payload)
     }
 
-    fn parse_encrypted(&self, data: &[u8], payload_type: PayloadType) -> Option<Payload> {
-        let (&destination_hash, rest) = data.split_first()?;
+    fn parse_encrypted(&self, data: &[u8], payload_type: PayloadType) -> ParserResult<Payload> {
+        let (&destination_hash, rest) = data.split_first().ok_or(ParserError::UnexpectedEnd(
+            "encrypted",
+            "destination_hash",
+            1,
+        ))?;
         if destination_hash != self.identity.public[0] {
-            return Some(Payload::Undecryptable);
+            return Ok(Payload::Undecryptable);
         }
-        let (&source_hash, rest) = rest.split_first()?;
+        let (&source_hash, rest) =
+            rest.split_first()
+                .ok_or(ParserError::UnexpectedEnd("encrypted", "source_hash", 1))?;
         let ciphertext = rest;
         let Some(plaintext) = self
             .contacts
@@ -52,17 +61,36 @@ impl PayloadParser {
             .flat_map(|shared| decrypt(&shared, ciphertext))
             .next()
         else {
-            return Some(Payload::Undecryptable);
+            return Ok(Payload::Undecryptable);
         };
         let data = plaintext.as_slice();
         match payload_type {
             PayloadType::Path => {
-                let (&path_length, rest) = data.split_first()?;
-                let (path, rest) = rest.split_at_checked(path_length.into())?;
-                let path = heapless::Vec::from_slice(path).ok()?;
-                let (&extra_type, rest) = rest.split_first()?;
-                let extra = heapless::Vec::from_slice(rest).ok()?;
-                Some(Payload::Path {
+                const CONTEXT_NAME: &str = "encrypted->path";
+                let (&path_length, rest) = data.split_first().ok_or(ParserError::UnexpectedEnd(
+                    CONTEXT_NAME,
+                    "path_length",
+                    1,
+                ))?;
+                let (path, rest) =
+                    rest.split_at_checked(path_length.into())
+                        .ok_or(ParserError::UnexpectedEnd(
+                            CONTEXT_NAME,
+                            "path",
+                            path_length.into(),
+                        ))?;
+                let path = heapless::Vec::from_slice(path).map_err(|_| {
+                    ParserError::ExceedsCapacity(CONTEXT_NAME, "path", MAX_PATH_SIZE)
+                })?;
+                let (&extra_type, rest) = rest.split_first().ok_or(ParserError::UnexpectedEnd(
+                    CONTEXT_NAME,
+                    "extra_type",
+                    1,
+                ))?;
+                let extra = heapless::Vec::from_slice(rest).map_err(|_| {
+                    ParserError::ExceedsCapacity("encrypted->path", "extra", MAX_PACKET_PAYLOAD)
+                })?;
+                Ok(Payload::Path {
                     source_hash,
                     path,
                     extra_type,
@@ -70,34 +98,40 @@ impl PayloadParser {
                 })
             }
             PayloadType::Request => {
-                let (&timestamp, rest) = data.split_first_chunk::<4>()?;
+                const CONTEXT_NAME: &str = "encrypted->request";
+                let (&timestamp, rest) = data
+                    .split_first_chunk::<4>()
+                    .ok_or(ParserError::UnexpectedEnd(CONTEXT_NAME, "timestamp", 4))?;
                 let timestamp = u32::from_le_bytes(timestamp);
-                let (&request_type, rest) = rest.split_first()?;
-                let request_type = RequestType::try_from(request_type).ok()?;
+                let (&request_type, rest) = rest
+                    .split_first()
+                    .ok_or(ParserError::UnexpectedEnd(CONTEXT_NAME, "request_type", 1))?;
+                let request_type = RequestType::try_from(request_type)
+                    .map_err(|_| ParserError::BitParsingError(CONTEXT_NAME, "request_type"))?;
                 todo!()
             }
-            _ => None,
+            _ => Err(ParserError::UndefinedPayload),
         }
     }
-    fn parse_multipart(data: &[u8]) -> Option<Payload> {
+    fn parse_multipart(data: &[u8]) -> ParserResult<Payload> {
         let (&header, rest) = data.split_first()?;
         let remaining_packets = header >> 4;
         let payload_type = PayloadType::from(u4::new(header & 0b1111));
         let payload = rest;
-        Some(Payload::MultiPart {
+        Ok(Payload::MultiPart {
             remaining_packets,
             payload_type,
             payload: heapless::Vec::from_slice(payload).ok()?,
         })
     }
 
-    fn parse_ack(data: &[u8]) -> Option<Payload> {
+    fn parse_ack(data: &[u8]) -> ParserResult<Payload> {
         let (&crc, _) = data.split_first_chunk::<4>()?;
         let crc = u32::from_le_bytes(crc);
-        Some(Payload::Ack { crc })
+        Ok(Payload::Ack { crc })
     }
 
-    fn parse_control(data: &[u8]) -> Option<Payload> {
+    fn parse_control(data: &[u8]) -> ParserResult<Payload> {
         let (&header, rest) = data.split_first()?;
         let (&filter, rest) = rest.split_first()?;
         let filter = NodeTypeSet::from(filter);
@@ -131,10 +165,10 @@ impl PayloadParser {
             }
             ControlType::Unknown => None,
         }?;
-        Some(Payload::Control(control_data))
+        Ok(Payload::Control(control_data))
     }
 
-    fn parse_trace(data: &[u8]) -> Option<Payload> {
+    fn parse_trace(data: &[u8]) -> ParserResult<Payload> {
         let (&trace_tag, rest) = data.split_first_chunk::<4>()?;
         let trace_tag = u32::from_le_bytes(trace_tag);
         let (&auth_code, rest) = rest.split_first_chunk::<4>()?;
@@ -147,7 +181,7 @@ impl PayloadParser {
             2 => Path::from_3_byte_slice(rest),
             _ => None,
         }?;
-        Some(Payload::Trace {
+        Ok(Payload::Trace {
             trace_tag,
             auth_code,
             flags,
