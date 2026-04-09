8fa62b404bbdcdb3b460cfbd9d1fb24525cfac5f
Nareshkumar Rao <dev@nrx.sh>
2026-04-09 14:49:22 +0200

    wip

--- src/mesh/packet/encryption.rs
+++ src/mesh/packet/encryption.rs
@@ -1,23 +1,29 @@
-use hmac::{Hmac, KeyInit, Mac};
-use sha2::Sha256;
-
 use crate::mesh::packet::raw::MAX_PACKET_PAYLOAD;
 
 pub const CIPHER_KEY_SIZE: usize = 16;
-pub const CIPHER_MAC_SIZE: usize = 2;
 
-fn encrypt(secret: &[u8; 16], plaintext: &[u8]) -> Option<heapless::Vec<u8, MAX_PACKET_PAYLOAD>> {
+pub fn encrypt(
+    secret: &[u8; CIPHER_KEY_SIZE],
+    plaintext: &[u8],
+) -> Option<heapless::Vec<u8, MAX_PACKET_PAYLOAD>> {
     let mut ciphertext = aes_encrypt(secret, plaintext)?;
-    let mut padded = [0u8; 32]; // Initialize all zeros
-    padded[..16].copy_from_slice(secret);
-    let mac = Hmac::<Sha256>::new_from_slice(&padded).ok()?;
-    let (&mac, _) = mac
-        .finalize()
-        .into_bytes()
-        .split_first_chunk::<CIPHER_MAC_SIZE>()?;
+    let mac = gen_hmac(secret, &ciphertext)?;
     let mut result = heapless::Vec::from_array(mac);
     result.extend(ciphertext);
     Some(result)
+}
+
+pub fn decrypt(
+    secret: &[u8; CIPHER_KEY_SIZE],
+    ciphertext: &[u8],
+) -> Option<heapless::Vec<u8, MAX_PACKET_PAYLOAD>> {
+    let (&provided_hmac, ciphertext) = ciphertext.split_first_chunk::<CIPHER_MAC_SIZE>()?;
+    let calculated_hmac = gen_hmac(secret, ciphertext)?;
+    if provided_hmac != calculated_hmac {
+        return None;
+    }
+    let plaintext = aes_decrypt(secret, ciphertext)?;
+    Some(plaintext)
 }
 
 fn aes_encrypt(
@@ -58,6 +64,19 @@ fn aes_decrypt(
     Some(result)
 }
 
+const CIPHER_MAC_SIZE: usize = 2;
+fn gen_hmac(secret: &[u8], ciphertext: &[u8]) -> Option<[u8; CIPHER_MAC_SIZE]> {
+    use hmac::{Hmac, KeyInit, Mac};
+    use sha2::Sha256;
+    let mut mac = Hmac::<Sha256>::new_from_slice(secret).ok()?;
+    mac.update(ciphertext);
+    let (&mac, _) = mac
+        .finalize()
+        .into_bytes()
+        .split_first_chunk::<CIPHER_MAC_SIZE>()?;
+    Some(mac)
+}
+
 #[cfg(test)]
 mod tests {
 
@@ -65,17 +84,15 @@ mod tests {
 
     use super::*;
     #[test]
-    fn test_aes_decrypt() {
+    fn test_decrypt() {
         let secret = "80174f513e9099612b537bc1cd450a41";
         let secret = hex::decode(secret).unwrap();
-        let ciphertext = "343F31A462B35F2D79264F7CC1BADC880F400A68AE504EFE4CA85D69002E3E77";
+        let ciphertext = "E6C5343F31A462B35F2D79264F7CC1BADC880F400A68AE504EFE4CA85D69002E3E77";
         let ciphertext = hex::decode(ciphertext).unwrap();
-        let plaintext = aes_decrypt(secret[0..16].try_into().unwrap(), &ciphertext).unwrap();
-        dbg!(hex::encode(&plaintext));
-        let plaintext = CStr::from_bytes_until_nul(&plaintext[5..])
-            .unwrap()
-            .to_string_lossy();
-        assert_eq!("jgerhold-T114: Moin!", plaintext);
+        let plaintext = decrypt(&secret.try_into().unwrap(), &ciphertext).unwrap();
+        let expected = "da15d669006a676572686f6c642d543131343a204d6f696e2100000000000000";
+        let expected = hex::decode(expected).unwrap();
+        assert_eq!(expected.as_slice(), &plaintext);
     }
     #[test]
     fn test_encrypt() {
@@ -84,7 +101,8 @@ mod tests {
         let plaintext = "da15d669006a676572686f6c642d543131343a204d6f696e2100000000000000";
         let plaintext = hex::decode(plaintext).unwrap();
         let cipher = encrypt(secret[..16].try_into().unwrap(), &plaintext).unwrap();
-        dbg!(hex::encode(cipher));
-        //e6c5
+        let expected = "E6C5343F31A462B35F2D79264F7CC1BADC880F400A68AE504EFE4CA85D69002E3E77";
+        let expected = hex::decode(expected).unwrap();
+        assert_eq!(expected.as_slice(), &cipher);
     }
 }
